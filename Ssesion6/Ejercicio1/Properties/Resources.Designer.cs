7519b72d1ae830dee77fedd57cba0c6c6a9f832d
3DG4RDC26 <ercasco@uamv.edu.ni>
2024-09-06 18:23:09 -0600

    Sesion6

--- Ssesion6/Ejercicio1/Properties/Resources.Designer.cs
+++ Ssesion6/Ejercicio1/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace Ejercicio1.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap herramienta_de_copia {
+            get {
+                object obj = ResourceManager.GetObject("herramienta-de-copia", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
